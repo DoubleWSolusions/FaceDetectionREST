6e284355e0961b599ac60dad3cf3ae01c97950a7
Tomasz <tom.g2807@gmail.com>
2020-12-08 20:49:22 +0100

    django setup

---
+++ Backend/test.py
@@ -0,0 +1,27 @@
+from facenet_pytorch import MTCNN
+import cv2
+from PIL import Image
+import numpy as np
+from matplotlib import pyplot as plt
+from matplotlib import patches
+
+
+mtcnn = MTCNN(keep_all=True, device='cuda:0')
+
+# Load a single image and display
+frame = cv2.imread('../images/example7.jpg')
+frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+frame = Image.fromarray(frame)
+
+# Detect face
+boxes, probs, landmarks = mtcnn.detect(frame, landmarks=True)
+
+# Visualize
+fig, ax = plt.subplots(figsize=(16, 12))
+ax.imshow(frame)
+ax.axis('off')
+print(boxes)
+for box, landmark in zip(boxes, landmarks):
+    ax.scatter(*np.meshgrid(box[[0, 2]], box[[1, 3]]))
+    ax.scatter(landmark[:, 0], landmark[:, 1], s=8)
+plt.show()
