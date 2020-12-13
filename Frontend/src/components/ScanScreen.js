7f76e8285f7e0af07ffe674c1ede05f20fd3428f
Krzysztof Wicki <krzysztof_wicki@sevenet.pl>
2020-12-13 20:26:49 +0100

    commit

--- Frontend/src/components/ScanScreen.js
+++ Frontend/src/components/ScanScreen.js
@@ -13,10 +13,21 @@ import axios from 'axios';
 
 
 
+
+
 function ScanScreen (props) {
     const classes = useStyles();
+    const [stateGet, setStateGet] = useState({
+        isLoading: true,
+        users: [],
+        error: null,
+
+    })
+    const [stateID, setStateID] = useState({
+        id:'',
+    });
     const [state, setState] = useState({
-        name:'',
+        nazwa:'',
         profileImg:'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png',
         profileImg2:'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png'
     });
@@ -25,8 +36,17 @@ function ScanScreen (props) {
         imageAfter:'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png'
     });
 
+    const [componentState, setComponentState] = useState({
+        source: null,
+    })
+
     const handleChange = (e) => {
         setState({
+                [e.target.id]: e.target.value
+        })
+    };
+    const handleChangeID = (e) => {
+        setStateID({
                 [e.target.id]: e.target.value
         })
     };
@@ -58,7 +78,7 @@ function ScanScreen (props) {
     const handleSubmit = (e) => {
         e.preventDefault();
         let form_data = new FormData();
-        form_data.append('name', state.name);
+        form_data.append('name', state.nazwa);
         form_data.append('image', state.profileImg, state.profileImg.name);
         let url = 'http://fb7de35755e1.ngrok.io/api/image/';
         axios.post(url, form_data, {
@@ -74,35 +94,71 @@ function ScanScreen (props) {
 
     const handleGet = (e) => {
         e.preventDefault();
-        let url = 'http://fb7de35755e1.ngrok.io/api/detection3/?image=dziadek';
+        let url = 'http://fb7de35755e1.ngrok.io/api/detection3/?image=';
             axios({
                     method: 'get',
                     dataType: 'json',
-                    url: url,
+                    url: url + stateID.id,
                 
                 }).then((response) => {
                 console.log(response.data);
-                //console.log(response.data.image_after_detection);
                 setImgState({ 
-                    imageAfter : response.data.image_after_detection
+                    imageAfter : response.data.image_url
                 });
 
             }).catch((err=>{
                 console.log(err);
             }))
     }
-    
+
+    const componentDidMount = () => {
+        axios
+          .get(
+            'http://fb7de35755e1.ngrok.io/api/detection2/?image=dziadek',
+            { responseType: 'arraybuffer' },
+          )
+          .then(response => {
+            const base64 = btoa(
+              new Uint8Array(response.data).reduce(
+                (data, byte) => data + String.fromCharCode(byte),
+                '',
+              ),
+            );
+            setImgState({ imageAfter: "data:;base64," + base64 });
+          });
+        }
+
+
+    const atStart = ()=>{
+            axios.get('http://fb7de35755e1.ngrok.io/api/image/')
+                .then(response => 
+                responseData = JSON.parse(response.data),
+                responseData.map(user => ({
+                    id: `${user.id}`,
+                    name: `${user.name}`
+                })).then(users => {
+                    setStateGet({
+                        users,
+                        isLoading: false
+                    });
+                }).catch(error => setStateGet({ error, isLoading: false })));
+    }
+    atStart()
     return (
         <div>
+            
             <React.Fragment>
             <CssBaseline />
                 <Container fixed className={classes.containerB}>
                     <Grid container spacing = {3} className={classes.textPanel}>
                         <Grid item xs={12}>
-                            <hr className={classes.line}/>
-                            <Link to="/" className={classes.link}>
-                                <Button className={classes.newButton}>Wykrywanie twarzy</Button>
-                            </Link>   
+                        <hr className={classes.line}/>
+                            <Link to="/Scan" className={classes.link}>
+                                <Button className={classes.newButton}>Wykrywanie twojej twarzy</Button>
+                            </Link>    
+                            <Link to="/ScanAll" className={classes.link}>
+                                <Button className={classes.newButton}>Wykrywanie wszystkich twarzy</Button>
+                            </Link>                       
                             <hr className={classes.line}/>
                         </Grid>
                         <Grid item xs={4}>
@@ -116,18 +172,41 @@ function ScanScreen (props) {
                         <div className={classes.label} >                 
                         <form onSubmit={handleSubmit}>
                             <p>
-                                <input type="text" placeholder='Name' id='name' value={state.name} onChange={handleChange} required/>
+                                <input type="text" placeholder='Nazwa' id='nazwa' value={state.nazwa} onChange={handleChange} required/>
                             </p>
                             <p>
                                 <input type="file" id="input" accept="image/*" onChange={imageHandler} required/> 
                             </p>
                             <input type="submit" value="Wyślij" />
                         </form>
+                        <form>
+                            <p>
+                                <input type="text" placeholder='id' id='id' value={stateID.id} onChange={handleChangeID} required/>
+                            </p>
+                            <Button onClick={handleGet} className={classes.margintop}>zapisz id i wyśilj zapytanie</Button>
+                        </form>
+                        {!stateGet.isLoading ? (
+                            stateGet.users.map(user =>{
+                                const { id, name } = user;
+                                return (
+                                    <div key={id}>
+                                    <p>Name: {name}</p>
+                                    <hr />
+                                    </div>
+                                );
+                            })
+                        ):(
+                            <h3>Loading...</h3>
+                        )
+
+                        }
+                    
+
                         </div>
 
                         </Grid>
                         <Grid item xs={4}>
-                            <Button onClick={handleGet}>Przeprowadź detekcję</Button>
+                            <Button onClick={handleGet} className={classes.margintop}>Przeprowadź detekcję</Button>
                         </Grid>
                         <Grid item xs={4}>
                         <Card className={classes.cardStyle}>
@@ -196,6 +275,9 @@ function ScanScreen (props) {
         label:{
             marginTop: '1rem',
             textAlign: 'left',
+        },
+        margintop:{
+            marginTop: '7rem',
         },
       }));
   export default ScanScreen;
