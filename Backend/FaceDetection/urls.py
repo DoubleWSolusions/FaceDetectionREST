04fa7b713d63de23d673ce5461c953e3e490644c
Tomasz <tom.g2807@gmail.com>
2020-12-13 15:20:26 +0100

    creating different endpoints for detections

--- Backend/FaceDetection/urls.py
+++ Backend/FaceDetection/urls.py
@@ -8,5 +8,7 @@ from django.conf import settings
 
 urlpatterns = [
     path('detection/', views.DetectionView.as_view(), name='detection'),
+    path('detection2/', views.DetectionView2.as_view(), name='detection2'),
+    path('detection3/', views.DetectionView3.as_view(), name='detection3'),
     path('image/', views.ImageView.as_view(), name='image')
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_URL)
--- Backend/FaceDetection/views.py
+++ Backend/FaceDetection/views.py
@@ -1,16 +1,20 @@
+from django.core.files.base import ContentFile
+from django.core.files.uploadedfile import InMemoryUploadedFile
+from django.http import HttpResponse
+
 from .models import *
 from PIL import Image
 from .serializers import *
 from rest_framework.views import APIView
 from rest_framework.parsers import MultiPartParser, FormParser
 from rest_framework.response import Response
-from rest_framework import status
+from rest_framework import status, renderers
 from Model.find_faces import detect_face, to_bytes
-from django.core.files.uploadedfile import InMemoryUploadedFile
 from django.core.files import File
-
 import io
 import base64
+
+from Backend import settings
 
 
 class ImageView(APIView):
@@ -44,17 +48,55 @@ class DetectionView(APIView):
             im = Image.open(im_dir)
 
             faces = detect_face(im)
-            #faces.save(im_io, 'png')
-            #im_io.seek(0)
-            #im_io_png = base64.b64encode(im_io.getvalue())
-            #context = im_io_png.decode('UTF-8')
+            faces.save(im_io, 'png')
+            im_io.seek(0)
+            im_io_png = base64.b64encode(im_io.getvalue())
+            context = im_io_png.decode('UTF-8')
             # res = to_bytes(faces)
             #img_byte_arr = io.BytesIO()
             #faces.save(img_byte_arr, format='PNG')
-            #img_tag = mark_safe(f"data:image/png;base64, {context}")
 
-            #detection = Detection(image=image, image_after_detection=img_tag)
+            detection = Detection(image=image, image_after_detection=context)
+            serializer = DetectionSerializer(detection)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        except Exception as e:
+            print(e)
+            print('error')
+            return Response(status=status.HTTP_400_BAD_REQUEST)
 
+
+class DetectionView2(APIView):
+    parser_classes = (MultiPartParser, FormParser)
+
+    def get(self, request, *args, **kwargs):
+
+        try:
+            wich_image = request.query_params["image"]
+            image = ImageToDetection.objects.get(name=wich_image)
+            im_dir = '.' + image.image.url
+            im = Image.open(im_dir)
+            faces = detect_face(im)
+
+            return HttpResponse(faces, content_type="image/png")
+        except Exception as e:
+            print(e)
+            print('error')
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+
+
+class DetectionView3(APIView):
+    parser_classes = (MultiPartParser, FormParser)
+
+    def get(self, request, *args, **kwargs):
+
+        try:
+            im_io = io.BytesIO()
+            wich_image = request.query_params["image"]
+            image = ImageToDetection.objects.get(name=wich_image)
+            im_dir = '.' + image.image.url
+            im = Image.open(im_dir)
+
+            faces = detect_face(im)
             faces.save(im_io, 'JPEG')
             img_after_detect = File(im_io)
 
@@ -67,4 +109,3 @@ class DetectionView(APIView):
             print(e)
             print('error')
             return Response(status=status.HTTP_400_BAD_REQUEST)
-
